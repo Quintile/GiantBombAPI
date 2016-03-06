1af72145a0dd0e5978ea3960340de7558fa0603e
Jordan Grieve <quintilex@gmail.com>
2016-03-06 14:21:35 -0500

    added additional methods

--- src/Game.php
+++ src/Game.php
@@ -5,14 +5,43 @@ namespace Excessive\GiantBombAPI;
 class Game
 {
     protected $name;
+    protected $deck;
+    protected $id;
+
+    protected $images = [];
+    protected $platforms = [];
+
+    const IMAGE = 'http://static.giantbomb.com';
+
+    protected static $properties = [
+        'name', 'deck', 'id',
+    ];
 
     public function __construct(array $apiResponse)
     {
+        foreach(self::$properties as $property)
+            $this->$property = $apiResponse[$property];
+       
         $this->name = $apiResponse['name'];
+        foreach($apiResponse['platforms'] as $platform)
+            $this->platforms = new Platform($platform);
+
+        foreach($apiResponse['image'] as $image)
+            $this->images[] = self::IMAGE.$image;
+    }
+
+    public function getDeck()
+    {
+        return $this->deck;
     }
 
     public function getName()
     {
         return $this->name;
+    }
+
+    public function getPlatforms()
+    {
+        return $this->platforms;
     }
 }
---
+++ src/Platform.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Excessive\GiantBombAPI;
+
+class Platform
+{
+    protected $api_detail_url;
+
+    protected $id;
+
+    protected $name;
+
+    protected $site_detail_url;
+
+    protected $abbreviation;
+
+    protected static $properties = [
+        'api_detail_url',
+        'id',
+        'name',
+        'site_detail_url',
+        'abbreviation',
+    ];
+
+    public function __construct(array $apiResponse)
+    {
+        foreach(self::$properties as $property)
+            $this->$property = $apiResponse[$property];
+    }
+
+    public function getAbbreviation()
+    {
+        return $this->$abbreviation;
+    }
+}
